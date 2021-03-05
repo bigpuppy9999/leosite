79fee77f121be658022f65e06bffef92593419a2
Leo R <leorooney08@gmail.com>
2021-03-05 14:05:34 +0000

    t

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_URL = 'static/'
+STATIC_ROOT = "static/"
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
